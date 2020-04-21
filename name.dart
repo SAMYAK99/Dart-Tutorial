97192dcb105aa9cedfcc2fb27bff6d82b7681af1
Samyak Singh <54956876+SAMYAK99@users.noreply.github.com>
2020-04-22 01:36:03 +0530

    Create name.dart

---
+++ name.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+main (){
+  var   string  ;
+ stdout.writeln("Enter your  name "); 
+  string = stdin.readLineSync();
+ 
+    print("\n  Your name is $string");
+}
+
+
+
+/* output
+Enter your  name 
+stalker
+
+  Your name is stalker
+  */
