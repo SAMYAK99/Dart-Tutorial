144addb7eb63c3ca6adfb2186b4a1254e6be05c2
Samyak Singh <54956876+SAMYAK99@users.noreply.github.com>
2020-04-23 00:25:55 +0530

    Update List.dart

--- List.dart
+++ List.dart
@@ -1,1 +1,33 @@
+ // Colection - List()
+ void main(){
 
+   var  name1 = {"walker", "barry" , "tokyo" , "1" , "2.5"};
+
+  //length of List
+  print(name1.length); 
+
+  // Displaying each Variable 
+    for(var n in name1)
+      print(n); 
+
+   // copying
+    var name2 = [...name1];  // Spread Operator
+     name2[0] = "alan";
+     name2[4] = "4.5";
+        for(var n in name2)
+      print(n); 
+} 
+
+/*  Output
+5
+walker
+barry
+tokyo
+1
+2.5
+alan
+barry
+tokyo
+1
+4.5
+*/
