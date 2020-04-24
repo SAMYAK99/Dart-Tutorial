88e731a14919628f5216f757cc7f593c63a06d91
Samyak Singh <54956876+SAMYAK99@users.noreply.github.com>
2020-04-24 21:20:29 +0530

    Update functions.dart

--- functions.dart
+++ functions.dart
@@ -1,1 +1,21 @@
 
+// Functions - 1 
+
+dynamic sum (var num1 , var num2) => num1+num2;
+dynamic mul (var num3 , var num4) => num3*num4; 
+
+
+main(){
+
+print("Sum and Multiplication of 2 and 3 is -");
+  print(sum(2,3));
+  print(mul(2,3));
+
+}
+
+/* Output 
+Sum and Multiplication of 2 and 3 is -
+5
+6
+*/
+
