a02e1eb59bbe33e5842154c6a9f96b2820d48d67
Samyak Singh <54956876+SAMYAK99@users.noreply.github.com>
2020-04-24 21:21:26 +0530

    Update map.dart

--- map.dart
+++ map.dart
@@ -1,1 +1,26 @@
+//Collection - maps (Collection of key value pair of items)
+void main() {
 
+  var android = {
+    // any data type can be used as a key
+   // key     value
+       1   : "Lolipop" ,
+       2   :  "Oreo"  ,
+       3   :  "Pie"
+  };
+    print(android[2]);
+    
+   // Empty map 
+   var fruits = Map();
+    print(fruits); 
+
+    //Alternative
+   var fruit = {"1": "mango" , "2" : " banana"};
+     print(fruit["2"]);
+}
+
+/* Output 
+Oreo
+{}
+banana
+*/
