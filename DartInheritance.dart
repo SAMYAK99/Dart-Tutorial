3d973846d2901842ad84550f2d1cc0d7aa3d4e51
Samyak Singh <54956876+SAMYAK99@users.noreply.github.com>
2020-07-22 14:46:43 +0530

    Add files via upload

---
+++ DartInheritance.dart
@@ -0,0 +1,51 @@
+// Inheritance
+
+import 'dart:io';
+
+void main(List<String> args) {
+
+  var c  =Car();
+  c.name = "Active";
+  c.speed = 220.00 ;
+  print("\n Detais of Car are - ");
+  c.printName();
+  c.printspeed();
+
+ print("\n Detais of Bike are - ");
+var b  =Bike();
+  b.name = "Xcd 125";
+  b.speed = 240.00 ;
+  b.printName();
+  b.printspeed();
+
+
+}
+
+class Vehicles {
+  String name  ;
+  double speed ;
+  
+    void printName(){
+      print(name);
+    }
+    void printspeed(){
+      print(speed);
+    }
+}
+
+class Car extends Vehicles {
+  
+}
+
+class Bike extends Vehicles{
+
+}
+
+// Output 
+//  Detais of Car are - 
+// Active
+// 220.0
+
+//  Detais of Bike are -
+// Xcd 125
+// 240.0
